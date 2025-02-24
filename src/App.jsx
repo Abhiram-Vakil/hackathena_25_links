e908829e257c58ce076d89d8243f472c03b8912e
Alan Jose Santo <realityaudiostudio@gmail.com>
2025-02-24 20:53:51 +0530

    Update links in App component and comment out unused state

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import logo from './assets/hlogo.svg'
 import squid from "./assets/squid.svg"
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className='linkpage'>
@@ -14,11 +14,12 @@ function App() {
       </div>
       <div className='buttons'>
         <button onClick={() => window.open("https://hackathena-25.vercel.app/", "_blank")}>Website</button>
-        <button onClick={() => window.open("https://your-link.com", "_blank")}>Judge App</button>
+        <button onClick={() => window.open("https://an-alan-musical.notion.site/Judge-App-1a451052c9cb80e3bc0fc106b129bb33", "_blank")}>Judge App</button>
         <button onClick={() => window.open("https://hackathena-25-students.vercel.app/login", "_blank")}>Participants App</button>
+        <button onClick={() => window.open("https://an-alan-musical.notion.site/Hackathena-Participant-Verification-App-1a451052c9cb806b9de2fb6169c17f5d", "_blank")}>Verification App</button>
         <button onClick={() => window.open("https://t.me/hackathena_25bot", "_blank")}>Telegram Bot</button>
-        <button onClick={() => window.open("https://t.me/hackathena_bot", "_blank")}>Shop Bot</button>
-        <button onClick={() => window.open("https://your-link.com", "_blank")}>Developer Info</button>
+        <button onClick={() => window.open("https://an-alan-musical.notion.site/ShopBot-1a451052c9cb80c291cfc2524bd4895c", "_blank")}>Shop Bot</button>
+        <button onClick={() => window.open("https://an-alan-musical.notion.site/Meet-the-Developers-Behind-Hackathena-Bundles-1a451052c9cb80d0b18fe721be2e115d", "_blank")}>Developer Info</button>
       </div>
       <img src={squid} alt="" className='squid' />
     </div>

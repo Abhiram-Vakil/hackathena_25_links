0726f179ce3b0ab576b2c116573672a1914beb60
Abhiram-Vakil <abhiramvakil@gmail.com>
2025-02-24 10:19:35 +0530

    added links

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import logo from './assets/hlogo.svg'
+import squid from "./assets/squid.svg"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='linkpage'>
+      <img src={logo} alt="" className='hlogo'/>
+      <div className='title'>
+        <h1>BUNDLES</h1>
+        <p>The Ultimate Software Set !</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='buttons'>
+        <button onClick={() => window.open("https://hackathena-25.vercel.app/", "_blank")}>Website</button>
+        <button onClick={() => window.open("https://your-link.com", "_blank")}>Judge App</button>
+        <button onClick={() => window.open("https://hackathena-25-students.vercel.app/login", "_blank")}>Participants App</button>
+        <button onClick={() => window.open("https://t.me/hackathena_25bot", "_blank")}>Telegram Bot</button>
+        <button onClick={() => window.open("https://t.me/hackathena_bot", "_blank")}>Shop Bot</button>
+        <button onClick={() => window.open("https://your-link.com", "_blank")}>Developer Info</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <img src={squid} alt="" className='squid' />
+    </div>
   )
 }
 
